ab4939d217e6f79a142b33e2d6d0a4c9003e6172
jcstotomas <jcstotomas@yahoo.com>
2020-01-06 14:04:18 -0800

    update user

--- app/api/users.py
+++ app/api/users.py
@@ -1,7 +1,9 @@
 from app.api import bp
 from app.models import User
 from flask import jsonify
-
+from app.api.errors import bad_request
+from app import db
+from flask import url_for, request
 @bp.route('/users/<string:username>', methods=['GET'])
 def get_user(username):
     return jsonify(User.query.filter_by(username=username).first_or_404().user_to_dict())
@@ -20,8 +22,34 @@ def get_followed(id):
 
 @bp.route('/users', methods=['POST'])
 def create_user():
-    pass
+    data = request.get_json() or {}
+    print(data)
+    if 'username' not in data or 'email' not in data or 'password' not in data:
+        return bad_request("Must include username, password, and email")
+    if User.query.filter_by(username=data['username']).first():
+        return bad_request("Username already exists")
+    if User.query.filter_by(email=data['email']).first():
+        return bad_request("Email already exists")
+
+    user = User()
+    user.from_dict(data, new_user=True)
+    db.session.add(user)
+    db.session.commit()
+    response = jsonify(user.user_to_dict())
+    response.status_code = 201 
+    response.headers['Location'] = url_for('api.get_user', username=user.username)
+    return response
+    
+
 
 @bp.route('/users/<string:username>', methods=['PUT'])
-def update_user(id):
-    pass
+def update_user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    data = request.get_json() or {}
+    if 'username' in data and data['username'] != user.username and User.query.filter_by(username=data['username']).first():
+        return bad_request('please change username')
+    if 'email' in data and data['email'] != user.email and User.query.filter_by(email=data['email']).first():
+        return bad_request('please use a different email address')
+    user.from_dict(data, new_user=False)
+    db.session.commit()
+    return jsonify(user.user_to_dict())
